9e91a77da76a2c14196c599e26a877373a8b5706
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-02 11:43:44 +0300

    backend <> created user migration

---
+++ backend/laravel-backend/database/migrations/2022_10_02_081616_create_user_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('age');
+            $table->string('gender');
+            $table->string('interest');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->tinyInteger('private')->default('0');
+            $tale->remeberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user');
+    }
+};
