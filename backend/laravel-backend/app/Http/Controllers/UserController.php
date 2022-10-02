224faa7831ac1b1ae282553916fb54b9b2fa9d01
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-02 12:00:34 +0300

    backened <> signup function user controller

--- backend/laravel-backend/app/Http/Controllers/UserController.php
+++ backend/laravel-backend/app/Http/Controllers/UserController.php
@@ -4,7 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\User;
+
 class UserController extends Controller
 {
-    //
+    function signUp(Request $request){
+        $this->validate($request,[
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+        ]);
+
+        $user= new User([
+            'name'=$request->input("name"),
+            'email'=$request->input("email"),
+            'password'=bcrypt($request->input("password")),
+        ]);
+
+        $user->save();
+        return response()->json([
+            "message"=>"Success"
+        ]);
+    }
 }
