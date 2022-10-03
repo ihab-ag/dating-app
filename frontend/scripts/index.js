8da5eb1fc36cfe26f6be8f0459818619e020ffa0
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-03 21:32:34 +0300

    frontend <> get location functionality

---
+++ frontend/scripts/index.js
@@ -0,0 +1,27 @@
+// DOM elements
+const location_btn= document.getElementById('location-btn');
+const signup_form= document.getElementById('signup');
+const longitude= signup_form.elements.longitude;
+const latitude= signup_form.elements.latitude;
+const validation_msg= document.getElementById('validation');
+
+// functions
+// get geolocation from browser
+getLocation=()=>{
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+      validation_msg.innerHTML = "Geolocation is not supported by this browser.";
+    }
+  }
+// get position values
+function showPosition(position) {
+    longitude.value= position.coords.longitude;
+    latitude.value= position.coords.latitude;
+  }
+
+// events
+location_btn.onclick=(e)=>{
+    e.preventDefault();
+    getLocation();
+}
