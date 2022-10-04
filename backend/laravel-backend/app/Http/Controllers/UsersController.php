552e0051e0109ce1b6d29cd0d8b046f02e6fc747
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-04 20:01:29 +0300

    backend <> created users controller

---
+++ backend/laravel-backend/app/Http/Controllers/UsersController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use App\Models\Location;
+use App\Models\Picture;
+
+class UsersController extends Controller
+{
+    
+}
